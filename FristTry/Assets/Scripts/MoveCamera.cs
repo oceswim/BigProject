f6dc13f82439dc19c859726502c4a20fbfb4f1fc
ocesiwm <oceaneperetti@gmail.com>
2020-02-25 17:46:36 +0000

    pictures taken properly

--- FristTry/Assets/Scripts/MoveCamera.cs
+++ FristTry/Assets/Scripts/MoveCamera.cs
@@ -7,44 +7,34 @@ public class MoveCamera : MonoBehaviour
     private Vector3 target = new Vector3(0, 0, 0);
     private int resWidth = 256;
     private int resHeight = 256;
-    public Camera snapCam;
+    private Camera snapCam;
     int index;
     // Start is called before the first frame update
     void Start()
     {
+        snapCam = GetComponent<Camera>();
         index = 0;
         increment = GameManager.step3;
         azimAngle = GameManager.azimuthMaxAngle;
         elevationAngle = GameManager.elevationMaxAngle;
         distance = GameManager.objMaxDist;
         transform.position = new Vector3(transform.position.x, transform.position.y,distance);
-        transform.Rotate(0, elevationAngle, 0);
+        transform.Rotate(elevationAngle, 0, 0);
+        RotateAndCapture();
+    }
+    private void RotateAndCapture() // for loop based on elevation angle + azim angle + dist value
+    {
+       
         StartCoroutine(TakePicture());
     }
-
     // Update is called once per frame
     private IEnumerator TakePicture()
     {
-        transform.RotateAround(target, Vector3.up, increment);
-
-        snapCam.gameObject.SetActive(true);
-        yield return new WaitForSeconds(1);
+        string path = "Images/" + GameManager.projectName + "/Img" + index.ToString();
+        index++;
+        ScreenCapture.CaptureScreenshot(path);
+        yield return new WaitForEndOfFrame();
     }
-    void LateUpdate()
-    {
-        if (snapCam.gameObject.activeInHierarchy)
-        {
-            Texture2D snapshot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-            snapCam.Render();
-            RenderTexture.active = snapCam.targetTexture;
-            snapshot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-            byte[] bytes = snapshot.EncodeToPNG();
-            string fileName = "Images/" + GameManager.projectName + "/img" + index;
-            index++;
-            System.IO.File.WriteAllBytes(fileName, bytes);
-            Debug.Log("snapshot taken");
-            snapCam.gameObject.SetActive(false);
-        }
-        
-    }
-}
+    
+    
+}//for loop from min value to max value avec increment pour i++;
